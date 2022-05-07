a0ad1022a1ce56ece0356114d85ad331d47cae76
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2022-05-07 14:57:18 -0400

    Update

--- script.js
+++ script.js
@@ -39,4 +39,34 @@ function moveDown(){
     }
 }
 
+// FUNÇÃO ATIRA
+function fireLaser(){
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+function createLaserElement() {
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    let newLaser = document.createElement('img');
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser');
+    newLaser.style.left = `${xPosition}`;
+    newLaser.style.top = `${yPosition -10}`;
+    return newLaser;
+}
+
+function moveLaser(laser){
+    let laserInterval = setInterval(() => {
+        let xPosition = parseInt(laser.style.left);
+
+        if(xPosition === 340){
+            laser.remove()
+        }else{
+            laser.style.left = `${xPosition + 8}px`
+        }
+    }, 10)
+}
+
 window.addEventListener('keydown', FlyAhip);
