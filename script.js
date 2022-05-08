d3882de5db9b0e43321861a207104c63683c1fd8
Vinícius Oliveira <vinicius.oliveira1911@gmail.com>
2022-05-08 01:51:41 -0400

    Finishing...

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const yourShip = document.querySelector('.player');
 const playArea = document.querySelector('#main-play-area');
-const aliensImg = ['img/monster_1.png', 'img/monster_2.png', 'img/monster_3.png']
+const aliensImg = ['img/monster_1.png', 'img/monster_2.png', 'img/monster_3.png'];
+const instructionsText = document.querySelector('.game-instructions');
+const startButton = document.querySelector('.start-button')
+
 
 // MOVIMENTO E TIRO DA NAVE
-function FlyAhip(event){
+function FlyShip(event){
     if(event.key === 'ArrowUp'){
         event.preventDefault();
         moveUp();
@@ -128,5 +131,33 @@ function checkLaserCollision(laser, alien){
     }
 }
 
-window.addEventListener('keydown', FlyAhip);
-createAliens()
+// INICIO DO JOGO
+startButton.addEventListener('click', (event) =>{
+    playGame();
+})
+
+function playGame() {
+    startButton.style.display = 'none';
+    instructionsText.style.display= 'none';
+    window.addEventListener('keydown', FlyShip);
+    alienInterval = setInterval(() => {
+        createAliens();
+    }, 2000)
+}
+
+// GAME OVER
+
+function gameOver() {
+    window.removeEventListener('keydown', FlyShip);
+    clearInterval(alienInterval);
+    let aliens = document.querySelectorAll('.alien');
+    aliens.forEach((alien) => alien.remove());
+    let lasers = document.querySelectorAll('.laser');
+    lasers.forEach((laser) => laser.remove())
+    setTimeout(() => {
+        alert('FIM DE JOGO');
+        yourShip.style.top = "250px";
+        startButton.style.display = "block";
+        instructionsText.style.display = "block";
+    })
+}
